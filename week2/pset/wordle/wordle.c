55b7dc27aa5036c305f08c4725ba6978646cc72c
sydalwedaie <sydalwedaie@gmail.com>
2023-03-29 10:14:05 +0000

    PSET2: Wordle - Complete TODO #4

--- week2/pset/wordle/wordle.c
+++ week2/pset/wordle/wordle.c
@@ -102,6 +102,12 @@ int main(int argc, string argv[])
 
         // set all elements of status array initially to 0, aka WRONG
         // TODO #4
+        for (int j = 0; j < wordsize; j++)
+        {
+            status[j] = WRONG;
+            printf("%i ", status[j]); // REMOVE
+        }
+
 
         // Calculate score for the guess
         int score = check_word(guess, wordsize, status, choice);
